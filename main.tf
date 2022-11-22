8a6d2018b75c98a9f96dcacb32abacb844a17fcd
John Ojetunde <johnojetunde63@gmail.com>
2022-11-23 00:48:57 +0200

    pushed proxy changes

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_apigatewayv2_stage" "v1" {
 //  }
 
   route_settings {
-    route_key = "stagingv1"
+    route_key = "ANY /"
     data_trace_enabled = true
     detailed_metrics_enabled = true
   }
