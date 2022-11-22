5354a9ea31f98de4b9226e1a7e9c303157cfe962
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 22:27:52 +0200

    using environmental variable

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIAVGV47FPSHKRSQ652"
-//  var.AWS_ACCESS_KEY
-  secret_key = "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"
-  //var.AWS_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY
+  //"AKIAVGV47FPSHKRSQ652"
+  secret_key =  var.AWS_ACCESS_KEY
+
+  //"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"
   endpoints {
     sts = "https://sts.eu-west-2.amazonaws.com"
   }
