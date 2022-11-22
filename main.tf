3b798d6acc7d62068b39ac4047fb3a6c41667344
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 22:15:58 +0200

    eu sts endpoint

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "eu-west-2"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_ACCESS_KEY
+  endpoints {
+    sts = "https://sts.eu-west-2.amazonaws.com"
+  }
 }
 
 resource "aws_apigatewayv2_api" "example" {
