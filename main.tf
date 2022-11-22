0785e9212fd743228f42b6751eddf3686fab4400
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 23:38:32 +0200

    pushed proxy changes

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_apigatewayv2_integration" "v1" {
 
 resource "aws_apigatewayv2_route" "v1" {
   api_id    = aws_apigatewayv2_api.example.id
-  route_key = "ANY"
+  route_key = "ANY /"
 
   target = "integrations/${aws_apigatewayv2_integration.v1.id}"
 }
