258a8526d5f35ebe1fba4d3c5af4ced4d5277f27
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 22:48:06 +0200

    deploy api gateway

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 provider "aws" {
   region = "eu-west-2"
-//  access_key = var.AWS_ACCESS_KEY
-  //"AKIAVGV47FPSHKRSQ652"
-//  secret_key =  var.AWS_ACCESS_KEY
-
-  //"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"
   endpoints {
     sts = "https://sts.eu-west-2.amazonaws.com"
   }
@@ -13,4 +8,19 @@ provider "aws" {
 resource "aws_apigatewayv2_api" "example" {
   name          = "example-http-api"
   protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "example" {
+  api_id           = aws_apigatewayv2_api.example.id
+  integration_type = "HTTP_PROXY"
+
+  integration_method = "ANY"
+  integration_uri    = "https://developers.pleo.io/reference/introduction/{proxy}"
+}
+
+resource "aws_apigatewayv2_route" "example" {
+  api_id    = aws_apigatewayv2_api.example.id
+  route_key = "ANY /example/{proxy+}"
+
+  target = "integrations/${aws_apigatewayv2_integration.example.id}"
 }
