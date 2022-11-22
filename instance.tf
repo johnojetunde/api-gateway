47d4ce5e1fcbe873f44dd74af83843b09ee65b8a
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 21:48:37 +0200

    start up commit

---
+++ instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_ACCESS_KEY
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-02dc8ad50da58fffd"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
