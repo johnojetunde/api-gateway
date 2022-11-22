526b270db445643185265e769858e381ccc3f244
John Ojetunde <johnojetunde63@gmail.com>
2022-11-22 22:20:47 +0200

    eu sts endpoint

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
   region = "eu-west-2"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_ACCESS_KEY
+  access_key = "AKIAVGV47FPSHKRSQ652"
+//  var.AWS_ACCESS_KEY
+  secret_key = "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"
+  //var.AWS_ACCESS_KEY
   endpoints {
     sts = "https://sts.eu-west-2.amazonaws.com"
   }
